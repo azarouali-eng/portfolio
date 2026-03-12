7e6c03cdd818914e6b094d2b14f7c1b457053a83
Aymane Zarouali <a.zarouali@esisa.ac.ma>
2026-03-12 21:48:52 +0000

    Fix vercel adapter import path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'static',
